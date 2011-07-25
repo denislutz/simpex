bd16eed1af3b76d725e6f717a23bc0f3bd03e1a4
Denis Lutz <denis.lutz@gmail.com>
2011-07-25 09:37:10 +0200

    fixed catalog header

--- lib/simpex/catalog.rb
+++ lib/simpex/catalog.rb
@@ -4,7 +4,7 @@ class Catalog < TypeEntry
     super(@@type, values)
   end
 
-  @@type = Type.new("Catalog", %w{activeCatalogVersion(catalog(id),version) backRefPK buyer(uid) creationtime[forceWrite=true,dateformat=dd.MM.yyyy hh:mm:ss] defaultCatalog[allownull=true] id[unique=true,allownull=true] name[lang=de] name[lang=en] name[lang=fr] name[lang=it] owner(&Item) previewURLTemplate supplier(uid) urlPatterns})
+  @@type = Type.new("Catalog", %w{activeCatalogVersion(catalog(id),version) backRefPK buyer(uid) creationtime[forceWrite=true,dateformat=dd.MM.yyyy hh:mm:ss] defaultCatalog[allownull=true] id[unique=true,allownull=true] name[lang=de] name[lang=en] name[lang=fr] name[lang=it] owner(Customer.uid) previewURLTemplate supplier(uid) urlPatterns})
 
   def Catalog.type
     @@type
